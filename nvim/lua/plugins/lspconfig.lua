04631c1264b560d97c1307e09490caa031a56f8d
Stephen Molloy <sdmolloy@gmail.com>
2025-03-18 00:19:52 +0100

    C3

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -2,6 +2,7 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
       require("lspconfig").clangd.setup{}
+        require("lspconfig").c3_lsp.setup{}
     end
 }
 
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,15 @@
 local M = {
     "nvim-treesitter/nvim-treesitter",
+    config = function () 
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+          ensure_installed = { "c", "lua", "vim", "vimdoc" },
+          sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true },  
+        })
+    end,
     build = function()
         require("nvim-treesitter.install").update({ with_sync = true })()
     end,
